fb33d8a172a9e2d88babc3e38d5d6c232564c01f
Даниил Малыхин <dan9a_fact@mail.ru>
2024-02-22 01:03:09 +0300

    Добавьте файлы проекта.

---
+++ TimeLibrary/Class1.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+
+namespace TimeLibrary
+{
+    public class Class1
+    {
+        static Dictionary<int, string> months = new Dictionary<int, string>()
+        {
+            {1, "Января" },
+            {2, "Февраля" },
+            {3, "Марта" },
+            {4, "Апреля" },
+            {5, "Мая" },
+            {6, "Июня" },
+            {7, "Июля" },
+            {8, "Августа" },
+            {9, "Сентября" },
+            {10, "Октября" },
+            {11, "Ноября" },
+            {12, "Декабря" },
+        };
+
+        public static string differenceDate(DateTime start, DateTime late)
+        {
+            string strResult = "";
+            TimeSpan result = late - start;
+            strResult += "Разница в днях: " + result.TotalDays + Environment.NewLine;
+            strResult += "Разница в часах: " + result.TotalHours + Environment.NewLine;
+            return strResult;
+        }
+
+        public static string leapYear(DateTime year)
+        {
+            if (year.Year % 100 == 0 &&  year.Year % 400 == 0)
+            {
+                return "Високосный";
+            }
+            else if (year.Year % 4 == 0 && year.Year % 100 != 0)
+            {
+                return "Високосный";
+            }
+            return "Невисокосный";
+        }
+
+        public static string leapYear(int year)
+        {
+            if (year % 100 == 0 && year % 400 == 0)
+            {
+                return "Високосный";
+            }
+            if (year % 4 == 0 && year % 100 != 0)
+            {
+                return "Високосный";
+            }
+            return "Невисокосный";
+        }
+
+        public static string Timeofday(DateTime time)
+        {
+            if(time.Hour >= 0 && time.Hour < 6)
+            {
+                return "Ночь";
+            }
+            else if(time.Hour >= 6 && time.Hour < 12)
+            {
+                return "Утро";
+            }
+            else if(time.Hour >= 12 && time.Hour < 18)
+            {
+                return "День";
+            }
+            else if(time.Hour >= 18 && time.Hour < 24)
+            {
+                return "Вечер";
+            }
+            return "Ошибка";
+        }
+
+        public static string Timeofday(int time)
+        {
+            if (time >= 0 && time < 6)
+            {
+                return "Ночь";
+            }
+            else if (time >= 6 && time < 12)
+            {
+                return "Утро";
+            }
+            else if (time >= 12 && time < 18)
+            {
+                return "День";
+            }
+            else if (time >= 18 && time < 24)
+            {
+                return "Вечер";
+            }
+            return "Ошибка";
+        }
+
+        public static string FormatDate(DateTime date, string format)
+        {
+            if (format == "D")
+            {
+                return $"{date.Day} {months[date.Month]} {date.Year} г.";
+            }
+            if (format == "d")
+            {
+                return $"{date.Day}.{date.Month}.{date.Year}";
+            }
+            if (format == "G")
+            {
+                return $"{date.Day}.{date.Month}.{date.Year} {date.Hour}:{date.Minute}:{date.Second}";
+            }
+            return "Ошибка";
+        }
+    }
+}
---
+++ TimeLibraryTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using TimeLibrary;
+
+namespace TimeLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1 = new DateTime(2000, 9, 18);
+            DateTime date2 = new DateTime(2010, 9, 18);
+            Console.WriteLine(TimeLibrary.Class1.differenceDate(date1, date2));
+
+            Console.WriteLine(Class1.leapYear(new DateTime(2000, 12, 30)));
+            Console.WriteLine(Class1.leapYear(new DateTime(1900, 12, 30)));
+
+            Console.WriteLine(Class1.leapYear(2000));
+            Console.WriteLine(Class1.leapYear(1900));
+
+            Console.WriteLine(Class1.Timeofday(new DateTime(1900, 12, 31, 0, 0, 0))); //Ночь
+            Console.WriteLine(Class1.Timeofday(new DateTime(1900, 12, 31, 2, 0, 0))); //Ночь
+            Console.WriteLine(Class1.Timeofday(new DateTime(1900, 12, 31, 6, 0, 0))); //Утро
+            Console.WriteLine(Class1.Timeofday(new DateTime(1900, 12, 31, 7, 0, 0))); //Утро
+            Console.WriteLine(Class1.Timeofday(new DateTime(1900, 12, 31, 12, 0, 0))); //День
+            Console.WriteLine(Class1.Timeofday(new DateTime(1900, 12, 31, 17, 0, 0))); //День
+            Console.WriteLine(Class1.Timeofday(new DateTime(1900, 12, 31, 18, 0, 0))); //Вечер
+            Console.WriteLine(Class1.Timeofday(new DateTime(1900, 12, 31))); //Ночь
+
+            Console.WriteLine(Class1.FormatDate(new DateTime(), ""));
+            Console.WriteLine(Class1.FormatDate(new DateTime(2000, 12, 31), ""));
+            Console.WriteLine(Class1.FormatDate(new DateTime(2000, 12, 31), "D"));
+            Console.WriteLine(Class1.FormatDate(new DateTime(2000, 12, 31), "d"));
+            Console.WriteLine(Class1.FormatDate(new DateTime(2000, 12, 31), "G"));
+        }
+    }
+}
